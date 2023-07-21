9293bf25e7b6cc6c437d8b371a28496e143fbbd2
Abraomukas <abraomukas@gmail.com>
2023-07-21 22:39:42 +0400

    Add components
    Need to finish the store, its configuration, the routes, and the basic structure of the pages.

---
+++ src/LayoutRoute.js
@@ -0,0 +1,11 @@
+import { Route } from 'react-router-dom';
+
+/**
+ * COMPONENTS
+ */
+
+function LayoutRoute(props) {
+	return <div></div>;
+}
+
+export default LayoutRoute;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import App from './App';
 import './index.css';
+import store from './store.js';
+
+const spinnerSizing = { width: '3rem', height: '3rem' };
+
+const spinnerFallback = (
+	<div className='d-flex justify-content-center mt-3'>
+		<div className='spinner-border' style={spinnerSizing} role='status'>
+			<span className='visually-hidden'>Loading...</span>
+		</div>
+	</div>
+);
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+	<React.Suspense fallback={spinnerFallback}>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.Suspense>,
+	document.getElementById('root')
 );

1d681c35131ee015872ba81f0f34a6ad8cffd450
Abraomukas <abraomukas@gmail.com>
2023-07-22 12:13:53 +0400

    Add Navbar
    WIP

---
+++ src/components/DropdownList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Translation } from 'react-i18next';
+
+function DropdownList(props) {
+	return (
+		<ul className='dropdown-menu' aria-labelledby='navbarDropdown'>
+			{props.dropdown.map((linkObj, index) => {
+				return (
+					<li key={index}>
+						<Translation>
+							{(t) => (
+								<Link className='dropdown-item' to={linkObj.path}>
+									{t(linkObj.label)}
+								</Link>
+							)}
+						</Translation>
+					</li>
+				);
+			})}
+		</ul>
+	);
+}
+
+export default DropdownList;
---
+++ src/components/Navbar.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import { Translation } from 'react-i18next';
+import i18next from 'i18next';
+
+/**
+ * COMPONENTS
+ */
+import DropdownList from './DropdownList';
+
+const bgColor = { backgroundColor: '#red' };
+const btnStyle = {
+	borderColor: '#8d323f',
+	borderWidth: 'medium',
+	borderStyle: 'solid',
+};
+
+const logoPath = './images/logo.png';
+const languages = [
+	{ name: 'English', country_code: 'gb' },
+	{ name: 'Español', country_code: 'es' },
+];
+
+function Navbar(props) {
+	const isLoggedIn = Cookies.get('isLogged') || false;
+	const currentLngCode = Cookies.get('i18next') || 'es';
+
+	return (
+		<header>
+			<div className='navbar fixed-top navbar-expand-lg' style={navbarBgColor}>
+				<div className='container-fluid'>
+					{/* BRAND */}
+
+					<div className='d-flex align-items-center'>
+						<Link className='navbar-brand' to='/'>
+							<img
+								src={logoPath}
+								height='55'
+								alt='easierSpeak'
+								loading='lazy'
+							/>
+						</Link>
+						<Translation>
+							{(t) => (
+								<Link className='navbar-brand text-white fw-bold' to='/'>
+									{t('title')} {isLoggedIn ? ' - ' + props.clubName : ''}
+								</Link>
+							)}
+						</Translation>
+					</div>
+				</div>
+			</div>
+		</header>
+	);
+}
+
+export default Navbar;

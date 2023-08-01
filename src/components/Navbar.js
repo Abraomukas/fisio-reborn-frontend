211b188cc25108be93cbecd3e1a54c1e82749023
Abraomukas <abraomukas@gmail.com>
2023-08-01 07:26:17 +0400

    Fix syntax

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,25 +28,25 @@ function Navbar(props) {
 
 	return (
 		<header>
-			<nav class='navbar navbar-expand-lg navbar-dark bg-dark'>
+			<nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
 				{/* CONTAINER WRAPPER */}
-				<div class='container-fluid'>
+				<div className='container-fluid'>
 					{/* TOGGLE BUTTON */}
 					<button
-						class='navbar-toggler'
+						className='navbar-toggler'
 						type='button'
 						data-mdb-toggle='collapse'
 						data-mdb-target='#navbarSupportedContent'
 						aria-controls='navbarSupportedContent'
 						aria-expanded='false'
 						aria-label='Toggle navigation'>
-						<i class='fas fa-bars'></i>
+						<i className='fas fa-bars'></i>
 					</button>
 
 					{/* COLLAPSIBLE WRAPPER */}
-					<div class='collapse navbar-collapse' id='navbarSupportedContent'>
+					<div className='collapse navbar-collapse' id='navbarSupportedContent'>
 						{/* BRAND */}
-						<a class='navbar-brand mt-2 mt-lg-0' href='/'>
+						<a className='navbar-brand mt-2 mt-lg-0' href='/'>
 							<img
 								src='./images/navbar-logo.png'
 								height='70'
@@ -55,19 +55,19 @@ function Navbar(props) {
 							/>
 						</a>
 						{/* LEFT LINKS */}
-						<ul class='navbar-nav me-auto mb-2 mb-lg-0'>
-							<li class='nav-item'>
-								<a class='nav-link' href='#'>
+						<ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+							<li className='nav-item'>
+								<a className='nav-link' href='#'>
 									Dashboard
 								</a>
 							</li>
-							<li class='nav-item'>
-								<a class='nav-link' href='#'>
+							<li className='nav-item'>
+								<a className='nav-link' href='#'>
 									Team
 								</a>
 							</li>
-							<li class='nav-item'>
-								<a class='nav-link' href='#'>
+							<li className='nav-item'>
+								<a className='nav-link' href='#'>
 									Projects
 								</a>
 							</li>
@@ -77,50 +77,50 @@ function Navbar(props) {
 					{/* COLLAPSIBLE WRAPPER */}
 
 					{/* RIGHT ELEMENTS */}
-					<div class='d-flex align-items-center'>
+					<div className='d-flex align-items-center'>
 						{/* ICON */}
-						<a class='text-reset me-3' href='#'>
-							<i class='fas fa-shopping-cart'></i>
+						<a className='text-reset me-3' href='#'>
+							<i className='fas fa-shopping-cart'></i>
 						</a>
 
 						{/* NOTIFICATIONS */}
-						<div class='dropdown'>
+						<div className='dropdown'>
 							<a
-								class='text-reset me-3 dropdown-toggle hidden-arrow'
+								className='text-reset me-3 dropdown-toggle hidden-arrow'
 								href='#'
 								id='navbarDropdownMenuLink'
 								role='button'
 								data-mdb-toggle='dropdown'
 								aria-expanded='false'>
-								<i class='fas fa-bell'></i>
-								<span class='badge rounded-pill badge-notification bg-danger'>
+								<i className='fas fa-bell'></i>
+								<span className='badge rounded-pill badge-notification bg-danger'>
 									1
 								</span>
 							</a>
 							<ul
-								class='dropdown-menu dropdown-menu-end'
+								className='dropdown-menu dropdown-menu-end'
 								aria-labelledby='navbarDropdownMenuLink'>
 								<li>
-									<a class='dropdown-item' href='#'>
+									<a className='dropdown-item' href='#'>
 										Some news
 									</a>
 								</li>
 								<li>
-									<a class='dropdown-item' href='#'>
+									<a className='dropdown-item' href='#'>
 										Another news
 									</a>
 								</li>
 								<li>
-									<a class='dropdown-item' href='#'>
+									<a className='dropdown-item' href='#'>
 										Something else here
 									</a>
 								</li>
 							</ul>
 						</div>
 						{/* AVATAR */}
-						<div class='dropdown'>
+						<div className='dropdown'>
 							<a
-								class='dropdown-toggle d-flex align-items-center hidden-arrow'
+								className='dropdown-toggle d-flex align-items-center hidden-arrow'
 								href='#'
 								id='navbarDropdownMenuAvatar'
 								role='button'
@@ -128,27 +128,27 @@ function Navbar(props) {
 								aria-expanded='false'>
 								<img
 									src='https://mdbcdn.b-cdn.net/img/new/avatars/2.webp'
-									class='rounded-circle'
+									className='rounded-circle'
 									height='25'
 									alt='Black and White Portrait of a Man'
 									loading='lazy'
 								/>
 							</a>
 							<ul
-								class='dropdown-menu dropdown-menu-end'
+								className='dropdown-menu dropdown-menu-end'
 								aria-labelledby='navbarDropdownMenuAvatar'>
 								<li>
-									<a class='dropdown-item' href='#'>
+									<a className='dropdown-item' href='#'>
 										My profile
 									</a>
 								</li>
 								<li>
-									<a class='dropdown-item' href='#'>
+									<a className='dropdown-item' href='#'>
 										Settings
 									</a>
 								</li>
 								<li>
-									<a class='dropdown-item' href='#'>
+									<a className='dropdown-item' href='#'>
 										Logout
 									</a>
 								</li>

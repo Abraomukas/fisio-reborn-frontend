fc003a450e81766c49fbee07f40dd75d6d3874d8
Abraomukas <abraomukas@gmail.com>
2023-08-06 21:19:23 +0400

    Remove notifications
    That space will has to accomdate the language selector

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -83,40 +83,8 @@ function Navbar(props) {
 							<i className='fas fa-shopping-cart'></i>
 						</a>
 
-						{/* NOTIFICATIONS */}
-						<div className='dropdown'>
-							<a
-								className='text-reset me-3 dropdown-toggle hidden-arrow'
-								href='#'
-								id='navbarDropdownMenuLink'
-								role='button'
-								data-mdb-toggle='dropdown'
-								aria-expanded='false'>
-								<i className='fas fa-bell'></i>
-								<span className='badge rounded-pill badge-notification bg-danger'>
-									1
-								</span>
-							</a>
-							<ul
-								className='dropdown-menu dropdown-menu-end'
-								aria-labelledby='navbarDropdownMenuLink'>
-								<li>
-									<a className='dropdown-item' href='#'>
-										Some news
-									</a>
-								</li>
-								<li>
-									<a className='dropdown-item' href='#'>
-										Another news
-									</a>
-								</li>
-								<li>
-									<a className='dropdown-item' href='#'>
-										Something else here
-									</a>
-								</li>
-							</ul>
-						</div>
+						{/* LANGUAGES */}
+						<div className='dropdown'></div>
 						{/* AVATAR */}
 						<div className='dropdown'>
 							<a

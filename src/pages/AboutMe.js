4cad73d0d259f14efe017a7e9369a801103addca
Abraomukas <abraomukas@gmail.com>
2023-10-12 23:15:27 +0100

    feat: About Me section beginnings

---
+++ src/pages/AboutMe.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Translation } from 'react-i18next';
+
+function AboutMe() {
+	return <div className='col-md-6 gx-5 mb-4'></div>;
+}

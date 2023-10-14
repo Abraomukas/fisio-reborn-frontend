210fca3ca4d9d6578aa10c395b9a682cf82a4145
Abraomukas <abraomukas@gmail.com>
2023-10-14 20:53:10 +0100

    feat: About Me section structure

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -3,5 +3,10 @@ import { Link } from 'react-router-dom';
 import { Translation } from 'react-i18next';
 
 function AboutMe() {
-	return <div className='col-md-6 gx-5 mb-4'></div>;
+	return (
+		<div className='row'>
+			<div className='col-md-6 gx-5 mb-4'></div>
+			<div className='col-md-6 gx-5 mb-4'></div>
+		</div>
+	);
 }

242c468380434450079ed79a60ae8588444eb244
Abraomukas <abraomukas@gmail.com>
2023-10-24 21:22:43 +0100

    feat: Add more sections to the Target section

--- src/pages/Target.js
+++ src/pages/Target.js
@@ -102,6 +102,68 @@ function Target() {
 					</div>
 				</div>
 			</div>
+			<div className='row gx-1'>
+				<div className='col-lg-4 col-md-12'>
+					<div className='card'>
+						<div
+							className='bg-image hover-overlay ripple'
+							data-mdb-ripple-color='light'>
+							<img
+								src='https://mdbootstrap.com/img/new/standard/nature/184.jpg'
+								className='img-fluid'
+							/>
+							<a href='#!'>
+								<div
+									className='mask'
+									style={{
+										backgroundColor: 'rgba(251, 251, 251, 0.15)',
+									}}></div>
+							</a>
+						</div>
+
+						<div className='card-body'>
+							<h5 className='card-title'>Card title</h5>
+							<p className='card-text'>
+								Some quick example text to build on the card title and make up
+								the bulk of the card's content.
+							</p>
+							<a href='#!' className='btn btn-primary'>
+								Button
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className='col-lg-4 col-md-12'>
+					<div className='card'>
+						<div
+							className='bg-image hover-overlay ripple'
+							data-mdb-ripple-color='light'>
+							<img
+								src='https://mdbootstrap.com/img/new/standard/nature/184.jpg'
+								className='img-fluid'
+							/>
+							<a href='#!'>
+								<div
+									className='mask'
+									style={{
+										backgroundColor: 'rgba(251, 251, 251, 0.15)',
+									}}></div>
+							</a>
+						</div>
+
+						<div className='card-body'>
+							<h5 className='card-title'>Card title</h5>
+							<p className='card-text'>
+								Some quick example text to build on the card title and make up
+								the bulk of the card's content.
+							</p>
+							<a href='#!' className='btn btn-primary'>
+								Button
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }

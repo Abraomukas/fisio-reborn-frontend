9f3b7c4bbe9d57058bd26f050faeb5ccc46d029e
Abraomukas <abraomukas@gmail.com>
2023-11-02 22:02:33 +0100

    wip: Add remaining code

--- src/pages/Testimonials.js
+++ src/pages/Testimonials.js
@@ -32,6 +32,65 @@ function Testimonials() {
 					/>
 				</div>
 			</div>
+
+			<div className='carousel-inner'>
+				{/* ITEM */}
+				<div className='carousel-item active'>
+					<img
+						src='https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp'
+						className='d-block w-100'
+						alt='Sunset Over the City'
+					/>
+					<div className='carousel-caption d-none d-md-block'>
+						<h5>First slide label</h5>
+						<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+					</div>
+				</div>
+
+				{/* ITEM */}
+				<div className='carousel-item'>
+					<img
+						src='https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp'
+						className='d-block w-100'
+						alt='Sunset Over the City'
+					/>
+					<div className='carousel-caption d-none d-md-block'>
+						<h5>First slide label</h5>
+						<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+					</div>
+				</div>
+
+				{/* ITEM */}
+				<div className='carousel-item'>
+					<img
+						src='https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(23).webp'
+						className='d-block w-100'
+						alt='Sunset Over the City'
+					/>
+					<div className='carousel-caption d-none d-md-block'>
+						<h5>First slide label</h5>
+						<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+					</div>
+				</div>
+			</div>
+
+			{/* CONTROLS */}
+			<button
+				className='carousel-control-prev'
+				type='button'
+				data-mdb-target='#carouselBasicExample'
+				data-mdb-slide='prev'>
+				<span className='carousel-control-prev-icon' aria-hidden='true'></span>
+				<span className='visually-hidden'>Previous</span>
+			</button>
+			<button
+				className='carousel-control-next'
+				type='button'
+				data-mdb-target='#carouselBasicExample'
+				data-mdb-slide='next'>
+				<span className='carousel-control-next-icon' aria-hidden='true'></span>
+				<span className='visually-hidden'>Next</span>
+			</button>
 		</div>
 	);
 }
